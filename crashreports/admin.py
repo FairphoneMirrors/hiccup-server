7c5f7ccf0944cdad31299316aa939e70e451830a
Dirk Vogt <dirk@fairphone.com>
2016-11-28 15:55:27 +0100

    HIC-51: register device, logfile and heartbeat model to admin

--- crashreports/admin.py
+++ crashreports/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
 from crashreports.models import Crashreport
+from crashreports.models import Device
+from crashreports.models import HeartBeat
+from crashreports.models import LogFile
+
 
 
 @admin.register(Crashreport)
+class CrashreportAdmin(admin.ModelAdmin):
+    pass
+    
+@admin.register(HeartBeat)
+class CrashreportAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(LogFile)
+class CrashreportAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Device)
 class CrashreportAdmin(admin.ModelAdmin):
     pass
