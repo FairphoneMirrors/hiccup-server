a10db9d3d738c630a73b158ee64c324ab7495883
Dirk Vogt <dirk@fairphone.com>
2017-05-05 09:08:45 +0200

    Add stability data to update history

--- crashreport_stats/raw_querys.py
+++ crashreport_stats/raw_querys.py
@@ -13,16 +13,29 @@ def fill_in_build_fingerprints(query, build_fingerprints):
 
 def execute_device_update_history_query(cursor, params):
     query = '''
-    SELECT 
+    SELECT
         min(crashreports_heartbeat.date) as update_date,
-        build_fingerprint
-    FROM 
+        build_fingerprint,
+        ( select count(crashreports_crashreport.id) from crashreports_crashreport
+          where  boot_reason in ("UNKNOWN", "keyboard power on")
+          and    crashreports_device.id == crashreports_crashreport.device_id
+          and    crashreports_crashreport.build_fingerprint == crashreports_heartbeat.build_fingerprint ) as prob_crashes,
+        ( select count(crashreports_crashreport.id) from crashreports_crashreport
+        where  boot_reason in ("RTC alarm")
+        and    crashreports_device.id == crashreports_crashreport.device_id
+        and    crashreports_crashreport.build_fingerprint == crashreports_heartbeat.build_fingerprint ) as SMPL,
+        ( select count(crashreports_crashreport.id) from crashreports_crashreport
+        where  boot_reason not in ("UNKNOWN", "keyboard power on", "RTC alarm")
+        and    crashreports_device.id == crashreports_crashreport.device_id
+        and    crashreports_crashreport.build_fingerprint == crashreports_heartbeat.build_fingerprint ) as other,
+        count(crashreports_heartbeat.id) as heartbeats
+    FROM
         crashreports_device
-    JOIN 
-        crashreports_heartbeat 
-    ON 
-        crashreports_device.id == crashreports_heartbeat.device_id 
-    where 
+    JOIN
+        crashreports_heartbeat
+    ON
+        crashreports_device.id == crashreports_heartbeat.device_id
+    where
         crashreports_device.uuid=%s
         group by build_fingerprint;
     '''
@@ -34,33 +47,33 @@ def execute_device_update_history_query(cursor, params):
 def execute_device_report_history(cursor, params):
     query = '''
     SELECT
-      strftime("%%Y-%%m-%%d",crashreports_heartbeat.date) as date, 
+      strftime("%%Y-%%m-%%d",crashreports_heartbeat.date) as date,
       count(crashreports_heartbeat.id) as heartbeats,
       (
-        select count(id) from crashreports_crashreport 
-        where 
-          boot_reason in ("RTC alarm")  
+        select count(id) from crashreports_crashreport
+        where
+          boot_reason in ("RTC alarm")
           and strftime("%%Y-%%m-%%d",crashreports_crashreport.date) == strftime("%%Y-%%m-%%d",crashreports_heartbeat.date)
           and crashreports_device.id == crashreports_crashreport.device_id
       ) as smpl,
       (
-        select count(id) from crashreports_crashreport 
-        where 
-          boot_reason in ("UNKNOWN", "keyboard power on") 
+        select count(id) from crashreports_crashreport
+        where
+          boot_reason in ("UNKNOWN", "keyboard power on")
           and strftime("%%Y-%%m-%%d",crashreports_crashreport.date) == strftime("%%Y-%%m-%%d",crashreports_heartbeat.date)
           and crashreports_device.id == crashreports_crashreport.device_id
         ) as prob_crashes,
         (
-        select count(id) from crashreports_crashreport 
-        where 
-          boot_reason not in ("RTC alarm", "UNKNOWN", "keyboard power on") 
+        select count(id) from crashreports_crashreport
+        where
+          boot_reason not in ("RTC alarm", "UNKNOWN", "keyboard power on")
           and strftime("%%Y-%%m-%%d",crashreports_crashreport.date) == strftime("%%Y-%%m-%%d",crashreports_heartbeat.date)
           and crashreports_device.id == crashreports_crashreport.device_id
         ) as other
-    from crashreports_device 
-    join 
-      crashreports_heartbeat on crashreports_device.id == crashreports_heartbeat.device_id 
-    where  
+    from crashreports_device
+    join
+      crashreports_heartbeat on crashreports_device.id == crashreports_heartbeat.device_id
+    where
       crashreports_device.uuid = %s
     group by date;
     '''
