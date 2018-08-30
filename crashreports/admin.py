786a170f902a6cc3357156b2e8a34c01910c50f8
Mitja Nikolaus <mitja@fairphone.com>
2018-08-30 16:14:14 +0200

    Replace crashreport admin management classes with register calls
    ModelAdmin classes are not needed if no custom values are defined:
    https://docs.djangoproject.com/en/1.11/ref/contrib/admin/#modeladmin-objects
    
    Issue: HIC-195
    Change-Id: I9838c01f3d7f9deeff82e63545783a5c87eab164

--- crashreports/admin.py
+++ crashreports/admin.py
@@ -6,30 +6,7 @@ from crashreports.models import Device
 from crashreports.models import HeartBeat
 from crashreports.models import LogFile
 
-
-@admin.register(Crashreport)
-class CrashreportAdmin(admin.ModelAdmin):
-    """Manage Crashreports as admin user."""
-
-    pass
-
-
-@admin.register(HeartBeat)
-class CrashreportAdmin(admin.ModelAdmin):
-    """Manage HeartBeats as admin user."""
-
-    pass
-
-
-@admin.register(LogFile)
-class CrashreportAdmin(admin.ModelAdmin):
-    """Manage LogFiles as admin user."""
-
-    pass
-
-
-@admin.register(Device)
-class CrashreportAdmin(admin.ModelAdmin):
-    """Manage Devices as admin user."""
-
-    pass
+admin.site.register(Crashreport)
+admin.site.register(HeartBeat)
+admin.site.register(LogFile)
+admin.site.register(Device)
