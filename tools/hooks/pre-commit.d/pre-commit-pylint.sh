bc75d05d08b1adb1a2fcc4c032c4f4bb6d3b4443
Mitja Nikolaus <mitja@fairphone.com>
2018-11-29 14:50:42 +0100

    Make pylint pre-commit hook strict
    Issue: HIC-275
    Change-Id: I2c0b3ae00b850f16faf632ca0105c0d177d257e4

--- tools/hooks/pre-commit.d/pre-commit-pylint.sh
+++ tools/hooks/pre-commit.d/pre-commit-pylint.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # For usage see
 # https://git-pylint-commit-hook.readthedocs.io/en/latest/usage.html
-git-pylint-commit-hook --always-show-violations
+git-pylint-commit-hook --always-show-violations --limit 10.0
